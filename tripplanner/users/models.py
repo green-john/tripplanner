7cc6d3094ca3bfb4395509efc08eb45a7584f1dc
Andres Ruiz <andresfeliperu@gmail.com>
2016-12-17 17:00:31 -0600

    Added basic user management

---
+++ tripplanner/users/models.py
@@ -0,0 +1,49 @@
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer, BadSignature, SignatureExpired
+
+from tripplanner import db, utils
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(255), nullable=False, index=True, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+
+    def __init__(self, username, raw_password):
+        self.username = username
+        self.password = utils.hash_password(raw_password)
+
+    def verify_password(self, raw_password):
+        return utils.verify_password(raw_password, self.password)
+
+    def generate_rest_auth_token(self, expiration=600):
+        """
+        Generates a token for the user to authenticate on REST requests.
+        :param expiration: duration of the validity of the token
+        :return: token for the user to authenticate
+        """
+        s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'],
+                                            expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def get_user_given_rest_token(token):
+        """
+        Returns the user given its encrypted token, otherwise
+        raises an error indicating the token is invalid or
+        has expired
+        :param token: encrypted auth token with user id
+        :return: user with the decrypted id or None if token is invalid
+        """
+        s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except BadSignature:
+            return None
+        except SignatureExpired:
+            return None
+
+        print(data)
+        user = User.query.get(data['id'])
+        return user
---
+++ tripplanner/users/views.py
@@ -0,0 +1,73 @@
+from flask import Blueprint, request, jsonify, url_for, g
+from flask_restful import abort
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+
+from tripplanner import db
+from tripplanner.users.models import User
+
+
+user_app = Blueprint('user', __name__)
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth(scheme='Token')
+
+
+@user_app.route('/users', methods=['POST'])
+def register_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    user = User(username, password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'username': username}), 201, {'Location': url_for('get_user', _id=user.id, _external=True)}
+
+
+@user_app.route('/users/<int:_id>/')
+def get_user(_id):
+    user = User.query.get(_id)
+    if not user:
+        abort(400)
+
+    return jsonify({'username': user.username})
+
+
+@user_app.route('/users/')
+def all_users():
+    users = User.query.all()
+    return jsonify([{'username': u.username} for u in users])
+
+
+@user_app.route('/users_auth/', methods=['POST', 'GET'])
+@token_auth.login_required
+def all_users_auth():
+    users = User.query.all()
+    return jsonify([{'username': u.username} for u in users])
+
+
+@user_app.route('/token/', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+    token = g.user.generate_rest_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+
+@basic_auth.verify_password
+def verify_password(username, password):
+    g.user = None
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+@token_auth.verify_token
+def verify_token(token):
+    g.user = None
+    u = User.get_user_given_rest_token(token)
+    if u:
+        g.user = u
+        return True
+    return False
+
