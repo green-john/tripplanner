3178bbe127d10c608e86302bbf791a50d6314659
Andres Ruiz <andresfeliperu@gmail.com>
2016-12-17 23:58:45 -0500

    added inits and changes to vies and models

---
+++ tripplanner/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+
+from tripplanner.config import config
+
+db = SQLAlchemy()
+mail = Mail()
+
+
+def create_app(config_name):
+    """
+    Creates the application with the specified configuration
+    :param config_name: name of the configuration to use
+    :return: application initialized
+    """
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    # Imports here to avoid circular imports
+    from tripplanner.users.views import user_app
+    from tripplanner.core.views import core_app
+    app.register_blueprint(core_app)
+    app.register_blueprint(user_app)
+
+    mail.init_app(app)
+    db.init_app(app)
+
+    return app
